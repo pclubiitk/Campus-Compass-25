0edbf0088f1d6ee076e2627d179869bd9bbab2e7
Himanshu-IITK <himanshukumarxia@gmail.com>
2025-06-28 18:31:32 +0530

    Converted my-map-app from submodule to regular folder

---
+++ Assignment/Assignment 2/map-ui-with-server/my-map-app/tailwind.config.ts
@@ -0,0 +1,13 @@
+// tailwind.config.ts
+export default {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
